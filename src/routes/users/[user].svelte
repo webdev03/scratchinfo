edfed0f4a54e7f3e0afe56e703ec906127bb75f7
webdev03 <75148774+webdev03@users.noreply.github.com>
2021-10-15 20:10:10 +1300

    Try catch 0

--- src/routes/users/[user].svelte
+++ src/routes/users/[user].svelte
@@ -150,14 +150,17 @@
             
             readForumChart = () => {
               let object = {};
-              console.log(object, forumlist.length)
               for (let index = 0; index < forumlist.length; index++) {
-                object[index] =
+                try {
+                  object[index] =
                   info.scratchdb.rawDataForum[forumlist[index]].count;
+                } catch {
+                  object[index] = 0;
+                }
+                
               }
               return object;
             };
-            console.log(readForumChart());
             // @ts-ignore
             var myChart = new Chart(ctx, {
               type: "pie",
@@ -174,7 +177,7 @@
                       "rgba(75, 192, 192, 1)",
                       "rgba(153, 102, 255, 1)",
                       "rgba(255, 159, 64, 1)",
-                      "rgba(255, 99, 132, 1)",
+                      "rgba(245, 19, 92, 1)",
                       "rgba(54, 162, 235, 1)",
                       "rgba(255, 206, 86, 1)",
                       "rgba(75, 192, 192, 1)",
@@ -185,7 +188,6 @@
                       "rgba(153, 102, 255, 1)",
                       "rgba(255, 159, 64, 1)",
                       "rgba(153, 102, 255, 1)",
-                      "rgba(255, 159, 64, 1)",
                     ],
                     borderWidth: 1,
                   },
@@ -193,7 +195,6 @@
               },
             });
           } catch(err) {
-            scratchdbProblem = true;
             throw err;
           }
         })
@@ -234,8 +235,7 @@
           }
         })
         .catch((error) => {
-          problem = true;
-          throw error;
+          
         });
     }
     fetchDataGroup();
