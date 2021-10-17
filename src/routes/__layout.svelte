93e086c2b3ec11847ae77153ee885616a36fa047
webdev03 <75148774+webdev03@users.noreply.github.com>
2021-10-17 14:47:03 +1300

    Add link to navbar

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -51,6 +51,9 @@
             <li class="nav-item">
               <a class="nav-link active" href="/about">About</a>
             </li>
+            <li class="nav-item">
+              <a class="nav-link active" href="/leaderboards">Leaderboards</a>
+            </li>
           </ul>
         </div>
       </div>
