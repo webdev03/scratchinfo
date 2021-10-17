7e2f4a80af02dd9ab3efdc2f5256bcf93e5cdd1a
webdev03 <75148774+webdev03@users.noreply.github.com>
2021-10-17 14:45:03 +1300

    first commit to leaderboard

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -10,6 +10,8 @@
     integrity="sha384-kQtW33rZJAHjgefvhyyzcGF3C5TFyBQBA13V1RKPf4uH+bwyzQxZ6CmMZHmNBEfJ"
     crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.0/font/bootstrap-icons.css">
+  <title>ScratchInfo | Scratch statistics!</title>
 </svelte:head>
 
 <script context="module">
---
+++ src/routes/leaderboards/followers.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+    let mostFollowed: Array<object> = [{username: "griffpatch"}];
+    let problem: boolean = false;
+    let loading: boolean = true;
+    onMount(() => {
+        fetch("https://scratchdb.lefty.one/v3/user/rank/global/followers").then((res) => {
+            if(!res.ok) {
+                problem = true;
+            } else {
+                return res.json();
+            }
+        }).then((data) => {
+            mostFollowed = data;
+            loading = false;
+        })
+    })
+</script>
+<h1>Most Followers Leaderboard</h1>
+{#if loading == false}
+{#each mostFollowed as user, position}
+    <div class="user rounded">#{position + 1}: {user["username"]}</div>
+{/each}
+{:else}
+    Loading...
+{/if}
+<style>
+    .user {
+        width: 100%;
+        background-color: lightblue;
+        min-height: 20px;
+        margin-bottom: 20px;
+        padding: 10px;
+        color: black;
+    }
+</style>
