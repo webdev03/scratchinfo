609c7f773de5187737483ff8a1f89f73b6885ee5
webdev03 <75148774+webdev03@users.noreply.github.com>
2021-10-19 19:08:01 +1300

    Fix double click bug

--- src/routes/leaderboards/index.svelte
+++ src/routes/leaderboards/index.svelte
@@ -1,10 +1,10 @@
 <h1>ScratchInfo Leaderboards</h1>
 <p>These are inspired by ScratchStats and ScratchDB.</p>
 <div class="leaderbar rounded">
-    <a href="./leaderboards/followers" class="btn btn-primary"><i class="bi bi-person-fill"/> Followers</a>
-    <a href="./leaderboards/views" class="btn btn-primary"><i class="bi bi-eye-fill"/> Views</a>
-    <a href="./leaderboards/loves" class="btn btn-primary"><i class="bi bi-heart-fill"/> Loves</a>
-    <a href="./leaderboards/favourites" class="btn btn-primary"><i class="bi bi-star-fill"/> Favourites</a>
+    <a href="/leaderboards/followers" class="btn btn-primary"><i class="bi bi-person-fill"/> Followers</a>
+    <a href="/leaderboards/views" class="btn btn-primary"><i class="bi bi-eye-fill"/> Views</a>
+    <a href="/leaderboards/loves" class="btn btn-primary"><i class="bi bi-heart-fill"/> Loves</a>
+    <a href="/leaderboards/favourites" class="btn btn-primary"><i class="bi bi-star-fill"/> Favourites</a>
 </div>
 
 <style>
