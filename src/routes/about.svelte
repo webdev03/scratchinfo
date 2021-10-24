57e98a5174413d95204da82bfb0f1d0caebd973a
webdev03 <75148774+webdev03@users.noreply.github.com>
2021-10-24 16:58:58 +1300

    ocular Ocular 2

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -9,6 +9,6 @@
   <a href="https://kit.svelte.dev">SvelteKit</a> which are both epic tools.
 </p>
 <p>I was inspired by @Jeffalo who made Ocular and @World_Languages who made ScratchStats.</p>
-<p>Information from ScratchDB by DatOneLefty, the official Scratch API, and Ocular by Jeffalo.</p>
+<p>Information from ScratchDB by DatOneLefty, the official Scratch API, and ocular by Jeffalo.</p>
 <p><b>Do you want to have your own Special on ScratchInfo? If so, contact @god286 on Scratch or go to <a href="https://scratch.mit.edu/discuss/topic/549251/">our forum post</a>.</b></p>
 
