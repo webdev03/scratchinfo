afecde0122c5b9c61eb67f5296071e069b19a0e5
webdev03 <75148774+webdev03@users.noreply.github.com>
2021-10-24 18:32:37 +1300

    one point o

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -3,6 +3,7 @@
   ScratchInfo is a project by @god286 on Scratch. It was made as a test to see
   if I could make a working website.
 </p>
+<p><b>WE JUST HIT V1.0.0 FOR SCRATCHINFO!</b> Thank you a lot for those who have helped contribute to scratchinfo. You all mean a lot to me.</p>
 <p>
   This website runs on <a href="https://svelte.dev">Svelte</a>
   and
@@ -11,4 +12,3 @@
 <p>I was inspired by @Jeffalo who made Ocular and @World_Languages who made ScratchStats.</p>
 <p>Information from ScratchDB by DatOneLefty, the official Scratch API, and Ocular by Jeffalo.</p>
 <p><b>Do you want to have your own Special on ScratchInfo? If so, contact @god286 on Scratch or go to <a href="https://scratch.mit.edu/discuss/topic/549251/">our forum post</a>.</b></p>
-
