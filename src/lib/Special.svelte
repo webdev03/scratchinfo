955293cf247d8ed51fbb39ef2b5e2bca79a4e523
webdev03 <75148774+webdev03@users.noreply.github.com>
2021-10-27 20:58:20 +1300

    Add more people's specials

--- src/lib/Special.svelte
+++ src/lib/Special.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
     export let info;
     let msg = "This is a ScratchInfo Special. It gives a little badge to people who do cool things!";
-    let helpers: Array<string> = [
+    let suggestors: Array<string> = [
         "Chiroyce",
+        "PoIygon",
+        "hamster46"
     ];
 </script>
 {#if info.username === "god286"}
@@ -17,8 +19,8 @@
 {#if info.scratchTeam === true}
     <div class="tag scratchteam" title={msg}>Scratch Team Member</div>
 {/if}
-{#if helpers.includes(info.username)}
-    <div class="tag helper" title={msg}>Helper</div>
+{#if suggestors.includes(info.username)}
+    <div class="tag suggestor" title={msg}>ScratchInfo Idea Suggestor</div>
 {/if}
 <style>
     .tag {
@@ -35,7 +37,7 @@
     .scratchteam {
         background-color: orange;
     }
-    .helper {
+    .suggestor {
         background-color: aquamarine;
     }
     .owner {
