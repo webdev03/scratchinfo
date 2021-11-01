90d99421ba6913c6d942a6752a0ca5bbeed0f29c
webdev03 <75148774+webdev03@users.noreply.github.com>
2021-11-01 16:42:52 +1300

    start transitioning to the scratchinfo api

--- src/routes/api/[user].ts
+++ src/routes/api/[user].ts
@@ -2,22 +2,40 @@
  * @type {import('@sveltejs/kit').RequestHandler}
  */
 export async function get({ params }) {
-  // the `slug` parameter is available because this file
-  // is called [slug].json.js
   const { user } = params;
-  const user_projects = await (await fetch(
-    `https://api.scratch.mit.edu/users/${user}/projects/?limit=1`
-  )).json();
-  const project_to_fetch = user_projects[0].id;
-  const project = await (await fetch(
-    `https://projects.scratch.mit.edu/${project_to_fetch}/`
-  )).json();
   try {
+    const api_official_user = await (
+      await fetch(`https://api.scratch.mit.edu/users/${user}/`)
+    ).json();
+    const scratchdb = await (
+      await fetch(`https://scratchdb.lefty.one/v3/user/info/${user}`)
+    ).json();
+    const ocular = await (
+      await fetch(`https://my-ocular.jeffalo.net/api/user/${user}/`)
+    ).json();
+    const user_projects = await (
+      await fetch(`https://api.scratch.mit.edu/users/${user}/projects/?limit=1`)
+    ).json();
+    const project_to_fetch = user_projects[0].id;
+    const project = await (
+      await fetch(`https://projects.scratch.mit.edu/${project_to_fetch}/`)
+    ).json();
+
     const agent = project["meta"]["agent"];
     return {
       body: {
+        username: api_official_user.username,
         user_agent: agent,
-        error: false
+        history: {
+          joined: api_official_user.history.joined,
+        },
+        ocular: {
+          status: ocular.status,
+          color: ocular.color,
+        },
+        scratchdb,
+        images: api_official_user.profile.images,
+        error: false,
       },
     };
   } catch (err) {
