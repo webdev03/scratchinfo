11be3ef58695f4d794fe8390dd7a7a798be153fc
webdev03 <75148774+webdev03@users.noreply.github.com>
2021-11-30 20:52:31 +1300

    add some more files

--- src/routes/scratchlight/verify.ts
+++ src/routes/scratchlight/verify.ts
@@ -1,9 +1,9 @@
 /**
  * @type {import('@sveltejs/kit').RequestHandler}
  */
-import { createClient } from "@supabase/supabase-js";
-import dotenv from "dotenv";
-import crypto from "crypto"
+import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
+import crypto from 'crypto';
 dotenv.config();
 
 export async function post(request) {
@@ -15,34 +15,37 @@ export async function post(request) {
       parsedBody = request.body;
     }
     const privateCode = parsedBody.privateCode;
-    if (typeof privateCode == "undefined") {
-      throw new Error("no private code")
+    if (typeof privateCode == 'undefined') {
+      throw new Error('no private code');
     }
-    const supabase = createClient(
-      process.env["SCRATCHLIGHT_URL"],
-      process.env["SCRATCHLIGHT_KEY"]
-    )
-    const getData = await supabase
-      .from('codes')
-      .select()
-      .eq("privateCode", privateCode)
+    const supabase = createClient(process.env['SCRATCHLIGHT_URL'], process.env['SCRATCHLIGHT_KEY']);
+    const getData = await supabase.from('codes').select().eq('privateCode', privateCode);
     if (getData.error || getData.data.length == 0) {
-      throw new Error("Cannot find session.")
+      throw new Error('Cannot find session.');
     }
-    const comments = await (await fetch(`https://api.scratch.mit.edu/users/ScratchLightAuth/projects/603838920/comments/?limit=15&offset=0&somethingCompletelyRandom=${crypto.randomBytes(24).toString('hex')}`)).json();
+    const comments = await (
+      await fetch(
+        `https://api.scratch.mit.edu/users/ScratchLightAuth/projects/603838920/comments/?limit=15&offset=0&somethingCompletelyRandom=${crypto
+          .randomBytes(24)
+          .toString('hex')}`
+      )
+    ).json();
     for (let index = 0; index < comments.length; index++) {
       const comment = comments[index];
-      if (comment.content == getData.data[0]["code"] && comment.author.username.toLowerCase() == getData.data[0]["user"].toLowerCase()) {
+      if (
+        comment.content == getData.data[0]['code'] &&
+        comment.author.username.toLowerCase() == getData.data[0]['user'].toLowerCase()
+      ) {
         const deleteAuthSession = await supabase
           .from('codes')
           .delete()
-          .eq("privateCode", privateCode);
+          .eq('privateCode', privateCode);
         return {
           body: {
             isError: false,
-            username: getData.data[0]["user"]
+            username: getData.data[0]['user']
           }
-        }
+        };
       }
     }
     return {
@@ -51,14 +54,14 @@ export async function post(request) {
         isError: true,
         msg: "We couldn't verify this auth session."
       }
-    }
+    };
   } catch {
     return {
       status: 500,
       body: {
         isError: true,
-        msg: "An error has occurred."
+        msg: 'An error has occurred.'
       }
-    }
+    };
   }
 }
