16167f1b5c8e1b19f5960979665c82b25b4edd44
webdev03 <75148774+webdev03@users.noreply.github.com>
2021-12-30 22:12:05 +1300

    add success and failure components

---
+++ src/lib/components/Failure.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let title = "Oh no!";
+  export let description = "It looks like an error has occurred.";
+</script>
+
+<div class="flex justify-center pt-2">
+  <div class="bg-red-500 text-white w-96 rounded p-2">
+    <h1 class="font-bold">{title}</h1>
+    <span>{description}</span>
+  </div>
+</div>
---
+++ src/lib/components/Success.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let title = "Success!";
+  export let description = "Your request has been fulfilled successfully..";
+</script>
+
+<div class="flex justify-center pt-2">
+  <div class="bg-green-500 text-white w-96 rounded p-2">
+    <h1 class="font-bold">{title}</h1>
+    <span>{description}</span>
+  </div>
+</div>
