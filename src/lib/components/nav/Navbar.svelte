fcf2df05ebd19e0f72df7ed9ac95549a9234849c
webdev03 <75148774+webdev03@users.noreply.github.com>
2022-01-07 18:56:13 +1300

    responsive navbar

--- src/lib/components/nav/Navbar.svelte
+++ src/lib/components/nav/Navbar.svelte
@@ -1,15 +1,15 @@
-<nav class="m-2 flex bg-gray-200 rounded-md align-bottom">
+<nav class="m-2 flex bg-gray-200 rounded-md align-bottom flex-1 flex-wrap">
   <div
     class="text-gray-800 mx-auto relative justify-between flex items-center w-full rounded min-h-50 px-4 py-2"
   >
     <!-- Header -->
     <h1 class="text-2xl font-semibold"><a href="/">Scratchinfo</a></h1>
     <!-- Link items -->
-    <ul class="flex space-x-2">
-      <li><a class="float-right flex justify-center hover:font-semibold hover:underline transition-all" href="/dashboard">Dashboard</a></li>
-      <li><a class="float-right flex justify-center hover:font-semibold hover:underline transition-all" href="/about">About</a></li>
-      <li><a class="float-right flex justify-center hover:font-semibold hover:underline transition-all" href="/leaderboards">Leaderboards</a></li>
-      <li><a class="float-right flex justify-center hover:font-semibold hover:underline transition-all" href="/collabs">Collabs</a></li>
+    <ul class="flex flex-wrap ml-2 flex-col sm:flex-row">
+      <li><a class="float-right flex justify-center hover:font-semibold hover:underline transition-all sm:m-2 m-0" href="/dashboard">Dashboard</a></li>
+      <li><a class="float-right flex justify-center hover:font-semibold hover:underline transition-all sm:m-2 m-0" href="/about">About</a></li>
+      <li><a class="float-right flex justify-center hover:font-semibold hover:underline transition-all sm:m-2 m-0" href="/leaderboards">Leaderboards</a></li>
+      <li><a class="float-right flex justify-center hover:font-semibold hover:underline transition-all sm:m-2 m-0" href="/collabs">Collabs</a></li>
     </ul>
   </div>
 </nav>
