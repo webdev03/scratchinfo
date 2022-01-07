e687116f94a6cda8244df5fc37fde269f3b72f17
webdev03 <75148774+webdev03@users.noreply.github.com>
2022-01-07 15:19:19 +1300

    remove unnecessary comment

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -13,42 +13,6 @@
 </svelte:head>
 <div class="bg-gray-900 pb-2 min-h-[100vh] min-w-[100vw] text-white">
   <div class="pt-3">
-    <!-- 
-    <nav class="flex md:top-0 md:z-50 rounded-md align-bottom">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="/">Scratchinfo</a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon" />
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" href="/">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link active" href="/dashboard">Dashboard</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link active" href="/about">About</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link active" href="/leaderboards">Leaderboards</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link active" href="/collabs">Collabs</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav> -->
     <Navbar />
     <div class="pt-3 p-4">
       <slot />
