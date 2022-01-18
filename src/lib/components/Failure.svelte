818fe88ab0b0912e7ac803a640aced193530871d
webdev03 <75148774+webdev03@users.noreply.github.com>
2022-01-18 19:30:20 +1300

    update font sizing

--- src/lib/components/Failure.svelte
+++ src/lib/components/Failure.svelte
@@ -6,7 +6,7 @@
 
 <div class="flex {centred ? "justify-center" : ""} pt-2">
   <div class="bg-red-500 text-white w-96 rounded p-2">
-    <h1 class="font-bold">{title}</h1>
+    <h3 class="font-bold text-2xl">{title}</h3>
     <span>{@html description}</span>
   </div>
 </div>
--- src/lib/components/Success.svelte
+++ src/lib/components/Success.svelte
@@ -5,7 +5,7 @@
 
 <div class="flex justify-center pt-2">
   <div class="bg-green-500 text-white w-96 rounded p-2">
-    <h1 class="font-bold">{title}</h1>
+    <h3 class="font-bold text-2xl">{title}</h3>
     <span>{@html description}</span>
   </div>
 </div>
