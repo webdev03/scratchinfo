45b9d7d0f574ceeaec855ecbcd16f7ae3205ce4f
webdev03 <75148774+webdev03@users.noreply.github.com>
2022-01-18 19:58:53 +1300

    modernify layout

--- src/routes/leaderboards/index.svelte
+++ src/routes/leaderboards/index.svelte
@@ -1,14 +1,14 @@
 <h1 class="text-3xl font-bold mb-2">Scratchinfo Leaderboards</h1>
 <p>These are inspired by ScratchStats and ScratchDB.</p>
-<div class="flex w-full min-h-[50px] flex-1 flex-wrap bg-gray-100 justify-center items-center space-x-2 p-1 rounded">
-  <a href="/leaderboards/followers" class="btn-primary"
+<div class="flex w-full min-h-[50px] flex-1 flex-wrap bg-gray-100 justify-center align-middle flex-col md:flex-row items-center space-x-2 p-1 rounded">
+  <a href="/leaderboards/followers" class="btn-primary sm:flex-1"
     ><i class="bi bi-person-fill" /> Followers</a
   >
-  <a href="/leaderboards/views" class="btn-primary"><i class="bi bi-eye-fill" /> Views</a>
-  <a href="/leaderboards/loves" class="btn-primary"><i class="bi bi-heart-fill" /> Loves</a>
-  <a href="/leaderboards/favourites" class="btn-primary"
+  <a href="/leaderboards/views" class="btn-primary sm:flex-1"><i class="bi bi-eye-fill" /> Views</a>
+  <a href="/leaderboards/loves" class="btn-primary md:flex-1"><i class="bi bi-heart-fill" /> Loves</a>
+  <a href="/leaderboards/favourites" class="btn-primary sm:flex-1"
     ><i class="bi bi-star-fill" /> Favourites</a
   >
-  <a href="/leaderboards/forums" class="btn-primary"><i class="bi bi-mailbox2" /> Forum Posts</a
+  <a href="/leaderboards/forums" class="btn-primary sm:flex-1"><i class="bi bi-mailbox2" /> Forum Posts</a
   >
 </div>
