90fffc1466ad43fdad32f767190acf03ea72a980
webdev03 <75148774+webdev03@users.noreply.github.com>
2022-01-18 19:40:00 +1300

    more modal changes

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -1,17 +1,16 @@
 <script>
-  export const title = 'You really sure?';
-  export const description = 'This could be a destructive action!';
-  export const cancelHook = () => {
-    document.getElementById("overlay-modal").remove()
-    document.getElementById("modal").remove()
-  };
-  export const okHook = () => {};
+  export let title = 'You really sure?';
+  export let description = 'This could be a destructive action!';
+  export let okHook;
+  export let cancelHook;
+
+  let id = Math.random().toString();
 </script>
 
-<div id="overlay-modal" class="absolute top-0 bottom-4 -m-4 min-w-full  opacity-30 bg-gray-500" />
+<div id="overlay-modal-{id}" class="absolute top-0 bottom-4 -m-4 min-w-full opacity-30 bg-gray-500" />
 
 <div
-  id="modal"
+  id="modal-{id}"
   class=" flex relative !opacity-100 mr-auto ml-auto left-0 min-h-[12rem] min-w-[30rem] right-0 justify-center pt-2"
 >
   <div class="bg-gray-100 text-gray-900 w-96 flex flex-col rounded shadow p-2">
