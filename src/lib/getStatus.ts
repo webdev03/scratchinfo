9e8f2c7783fc614fe58d5f76002ef1545040092c
webdev03 <75148774+webdev03@users.noreply.github.com>
2022-02-07 16:03:43 +1300

    prettier wasnt happy

--- src/lib/getStatus.ts
+++ src/lib/getStatus.ts
@@ -6,9 +6,9 @@ export async function getStatus(user: string) {
       'User-Agent': 'Mozilla/5.0'
     }
   });
-  
+
   const parsedBody = parse(await r.text());
 
-  const status = parsedBody.querySelector(".profile-details .group").innerText.trim();
+  const status = parsedBody.querySelector('.profile-details .group').innerText.trim();
   return status;
 }
