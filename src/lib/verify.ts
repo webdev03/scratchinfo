7a56ecb7b535908decd0e0d25ef31a7792f4cc1f
webdev03 <75148774+webdev03@users.noreply.github.com>
2022-06-05 19:14:17 +1200

    Reverse comments loading in ScratchLight
    Thank you https://github.com/Looky1173/Scratch-Auth-Server/commit/1808476689dd30533cbc9777afd1ef817ee5d2ac for the idea to patch this

--- src/lib/verify.ts
+++ src/lib/verify.ts
@@ -21,13 +21,13 @@ export async function verifier(request) {
     if (getData.error || getData.data.length == 0) {
       throw new Error("Cannot find session.");
     }
-    const comments = await (
+    const comments = (await (
       await fetch(
         `https://api.scratch.mit.edu/users/ScratchLightAuth/projects/603838920/comments/?limit=15&offset=0&somethingCompletelyRandom=${crypto
           .randomBytes(24)
           .toString("hex")}`
       )
-    ).json();
+    ).json()).reverse();
     for (let index = 0; index < comments.length; index++) {
       const comment = comments[index];
       if (
